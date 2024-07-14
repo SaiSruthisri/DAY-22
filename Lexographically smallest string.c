711904e6bd8e27fe9aa68c53705ffc0c073a96ec
Sai Sruthisri <148372065+SaiSruthisri@users.noreply.github.com>
2024-07-14 09:33:56 +0530

    Add files via upload

---
+++ Lexographically smallest string.c
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string getSmallestString(string s) {
+        int n=s.size();
+        for(int i=0;i<n-1;i++)
+        {
+            if((s[i]%2==0 && s[i+1]%2==0) && (s[i]>s[i+1]) )
+            {
+                swap(s[i],s[i+1]);
+                break;
+            }
+            if((s[i]%2!=0 && s[i+1]%2!=0) && (s[i]>s[i+1]))
+            {
+                swap(s[i],s[i+1]);
+                break;
+            }
+        }
+        return s;
+
+
+    }
+};
